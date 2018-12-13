39842c63727d793a57ba72d601b286edff142a60
caizhendi <75253500@qq.com>
2018-12-13 14:11:49 +0800

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import './index.scss';
 import registerServiceWorker from './register_service_worker';
 import AppRouter from './router';
-=======
-import App from './App';
-import './index.scss';
-import registerServiceWorker from './registerServiceWorker';
->>>>>>> 80a4140d6800d4d184fb99b9e846906a7bbfeda6
 
 ReactDOM.render(
   <AppRouter />,
